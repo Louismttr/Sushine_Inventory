1fb55c007ab38db31998e92a4ad40d723749e9c8
Luisa <110619581+Louismttr@users.noreply.github.com>
2024-11-20 08:45:48 -0600

    ClasesCompuestas (revisado por Nelio) xD

--- Sushine_lnventory/Models/Empleado.cs
+++ Sushine_lnventory/Models/Empleado.cs
@@ -8,10 +8,21 @@ namespace Sushine_lnventory.Models
 {
     public class Empleado : Persona
     {
+        
         public int Id { get; set; }
         public int EstadoEmpleado { get; set; }
         public DateTime FechaCont { get; set; }
         public Puesto Puesto { get; set; } // Composición
+
+        public Empleado(char cedula, string primer_nombre, string segundo_nombre, string primer_apellido, string segundo_apellido, char celular, char sexo,
+        int id, int estadoEmpleado, DateTime fechacont) :
+            base(cedula, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, celular, sexo)
+        {
+           this.Id = id;
+           this.EstadoEmpleado = estadoEmpleado;
+           this.FechaCont = fechacont;
+        }
+
 
     }
 }
--- Sushine_lnventory/Models/Persona.cs
+++ Sushine_lnventory/Models/Persona.cs
@@ -13,7 +13,19 @@ namespace Sushine_lnventory.Models
         public string Segundo_Nombre { get; set; }
         public string Primer_Apellido { get; set; }
         public string Segundo_Apellido { get; set; }
-        public char celular { get; set; }
-        public char sexo { get; set; }
+        public char Celular { get; set; }
+        public char Sexo { get; set; }
+
+        public Persona( char cedula, string primer_nombre, string segundo_nombre, 
+            string primer_apellido, string segundo_apellido, char celular, char sexo)
+        {
+            this.Cedula = cedula;
+            this.Primer_Nombre = primer_nombre;
+            this.Segundo_Apellido = segundo_nombre;
+            this.Primer_Apellido = primer_apellido;
+            this.Segundo_Apellido = segundo_apellido;
+            this.Celular = celular;
+            this.Sexo = sexo;
+        }
     }
 }
